5e1dd9402a0e096e19e0b684e2bc7c01b580aa89
gugun_guntur <gugunguntur99@gmail.com>
2020-11-04 07:21:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MileLogo from "../src/assets/img/mileapp.png";
+
+// reactstrap components
+import {
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Col,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Container,
+} from "reactstrap";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+      <main className={`bg`}>
+      <section className="section section-shaped section-lg">
+      <Container className="pt-lg-7" style={{ paddingTop: "100px", paddingBottom: "100px"  }}>
+        <Row className="justify-content-center">
+          <Col lg="5">
+            <Card className="bg-secondary shadow border-0">
+              <CardHeader className="bg-white pb-5">
+                <div className="text-muted text-center mb-3">
+                  {/* <small>Sign in with</small> */}
+                </div>
+                <div className="btn-wrapper text-center">
+                  <Button
+                    className="btn-neutral btn-icon"
+                    color="default"
+                    href="#pablo"
+                    onClick={e => e.preventDefault()}
+                  >
+                    <span className="btn-inner--icon mr-1">
+                      <img
+                        alt="..."
+                        src={MileLogo}
+                        // style={{ maxWidth: "300px", alignContent: "center" }}
+                      />
+                    </span>
+                  </Button>
+                    <p></p>
+                    <p></p>
+                    <span className="">Your one stop platform to manage all of your field service management</span>
+                </div>
+              </CardHeader>
+              <CardBody className="px-lg-5 py-lg-5 bg-white">
+                <Form role="form">
+                  <FormGroup className="mb-3">
+                    <InputGroup className="input-group-alternative">
+                      <Input placeholder="Enter your organization's name" type="text" />
+                    </InputGroup>
+                  </FormGroup>
+                  <div className="text-center">
+                    <Button
+                      className="my-2 w-100"
+                      color="primary"
+                      type="button"
+                    >
+                      Login
+                    </Button>
+                    <p></p>
+                    <span className="">Not registered yet? <span className={`App-link`}>Contact us</span> for more info</span>
+                  </div>
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      </section>
+      </main>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import Login from '../src/views/examples/Login';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
